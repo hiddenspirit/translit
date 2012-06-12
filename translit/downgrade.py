a69f8028cba8c3ae038e3f256ec7829b70d7a6d9
spirit <spirit@localhost>
2012-06-12 19:40:25 +0200

    Use re.U for Python 2 compatibility.

--- translit/downgrade.py
+++ translit/downgrade.py
@@ -3,13 +3,9 @@ from __future__ import print_function
 import builtins
 import codecs
 import os
+import re
 import sys
 import warnings
-
-try:
-    import regex as re
-except ImportError:
-    import re
 
 from . import DEFAULT_ENCODING
 from .unidecode import unidecode
@@ -30,7 +26,7 @@ else:
 
 
 RE_SUBS = {
-    ("«»", re.compile(r"«\s?|\s?»"), r'"'),
+    ("«»", re.compile(r"«\s?|\s?»", re.U), r'"'),
     ("Æ", re.compile(r"Æ([a-zß-öø-ÿœ])"), r"Ae\1"),
     ("Þ", re.compile(r"Þ([a-zß-öø-ÿœ])"), r"Th\1"),
     ("Œ", re.compile(r"Œ([a-zß-öø-ÿœ])"), r"Oe\1"),
@@ -67,22 +63,6 @@ def downgrade(text: str, encoding=DEFAULT_ENCODING) -> str:
     return _downgrade(text, encoding)
 
 
-def encode(text: str, encoding=DEFAULT_ENCODING) -> bytes:
-    """Encode text into a transliterated form.
-    """
-    return downgrade(text, encoding).encode(encoding)
-
-
-def print(*args, sep=" ", end="\n", file=sys.stdout): #@ReservedAssignment
-    """A print function that performs transliteration.
-    """
-    if file is not None:
-        encoding = file.encoding
-        if not encoding.startswith("utf"):
-            args = (downgrade(str(arg), encoding) for arg in args)
-    builtins.print(*args, sep=sep, end=end, file=file)
-
-
 if iconv_str:
     def _downgrade(text, encoding):
         result = []
@@ -95,6 +75,8 @@ if iconv_str:
                 except UnicodeEncodeError:
                     # Try iconv before using unidecode.
                     try:
+                        # TODO: Investigate why iconv from Python 2
+                        # behaves differently from Python 3 with \u202f.
                         nc = UNICODE_SUBS.get(c, c)
                         b = iconv_str(nc, encoding, "translit")
                     except IConvError:
@@ -133,3 +115,19 @@ else:
                 downgrade_cache[c, encoding] = repl
             result.append(repl)
         return "".join(result)
+
+
+def encode(text: str, encoding=DEFAULT_ENCODING) -> bytes:
+    """Encode text into a transliterated form.
+    """
+    return downgrade(text, encoding).encode(encoding)
+
+
+def print(*args, sep=" ", end="\n", file=sys.stdout): #@ReservedAssignment
+    """A print function that performs transliteration.
+    """
+    if file is not None:
+        encoding = file.encoding
+        if not encoding.startswith("utf"):
+            args = (downgrade(str(arg), encoding) for arg in args)
+    builtins.print(*args, sep=sep, end=end, file=file)
--- translit/upgrade.py
+++ translit/upgrade.py
@@ -1,16 +1,11 @@
 import locale
-#import unicodedata
+import re
 import warnings
-
-try:
-    import regex as re
-except ImportError:
-    import re
 
 try:
     from functools import lru_cache
 except ImportError:
-    lru_cache = None
+    from .backports.functools import lru_cache
 
 try:
     from . import spell
@@ -23,41 +18,41 @@ from . import DEFAULT_ENCODING
 
 TRANS_RE_SUBS = {
     "en": [
-        (re.compile(r"(?<!\w)'([\w\s,.'’]+?)'(?!\w)"), r"‘\1’"),
-        (re.compile(r"(\d)'"), r"\1′"),
+        (re.compile(r"(?<!\w)'([\w\s,.'’]+?)'(?!\w)", re.U), r"‘\1’"),
+        (re.compile(r"(\d)'", re.U), r"\1′"),
         (re.compile(r"'"), r"’"),
-        (re.compile(r'"(\w)'), r"“\1"),
-        (re.compile(r'^(?<!“)([^“]+\b\d+([.,]\d+)?)"'), r"\1″"),
+        (re.compile(r'"(\w)', re.U), r"“\1"),
+        (re.compile(r'^(?<!“)([^“]+\b\d+([.,]\d+)?)"', re.U), r"\1″"),
         (re.compile(r'"'), r"”"),
         (re.compile("«[ \xa0]"), "«\u202f"),
         (re.compile("[ \xa0]»"), "\u202f»"),
         (re.compile(r"--"), r"—"),
         (re.compile(r"\.{3}"), r"…"),
-        (re.compile(r"^[-–—]\s", re.M), "–\xa0"),
-        (re.compile(r"([^\d\w-])-([^\d\w-])"), r"\1–\2"),
-        (re.compile(r"(\d|\b)EUR(\d|\b)"), r"\1€\2"),
-        (re.compile(r"(\d|\b)GBP(\d|\b)"), r"\1£\2"),
-        (re.compile(r"(\d|\b)JPY(\d|\b)"), r"\1¥\2"),
+        (re.compile(r"^[-–—]\s", re.U | re.M), "–\xa0"),
+        (re.compile(r"([^\d\w-])-([^\d\w-])", re.U), r"\1–\2"),
+        (re.compile(r"(\d|\b)EUR(\d|\b)", re.U), r"\1€\2"),
+        (re.compile(r"(\d|\b)GBP(\d|\b)", re.U), r"\1£\2"),
+        (re.compile(r"(\d|\b)JPY(\d|\b)", re.U), r"\1¥\2"),
     ],
     "fr": [
-        (re.compile(r"(?<!\w)'([\w\s,.'’]+?)'(?!\w)"), r"‘\1’"),
-        (re.compile(r"(\d)'"), r"\1′"),
+        (re.compile(r"(?<!\w)'([\w\s,.'’]+?)'(?!\w)", re.U), r"‘\1’"),
+        (re.compile(r"(\d)'", re.U), r"\1′"),
         (re.compile(r"'"), r"’"),
-        (re.compile(r'"(\w)'), "«\u202f\\1"),
-        (re.compile(r'^(?<!«)([^«]+\b\d+([.,]\d+)?)"'), r"\1″"),
+        (re.compile(r'"(\w)', re.U), "«\u202f\\1"),
+        (re.compile(r'^(?<!«)([^«]+\b\d+([.,]\d+)?)"', re.U), r"\1″"),
         (re.compile(r'"'), "\u202f»"),
         (re.compile("«[ \xa0]"), "«\u202f"),
         (re.compile("[ \xa0]»"), "\u202f»"),
         (re.compile(r"--"), r"—"),
         (re.compile(r"\.{3}"), r"…"),
-        (re.compile(r"^[-–—]\s", re.M), "–\xa0"),
-        (re.compile(r"([^\d\w-])-([^\d\w-])"), r"\1–\2"),
-        (re.compile("(\\d)[ \xa0](\\d{3})\\b"), "\\1\u202f\\2"),
+        (re.compile(r"^[-–—]\s", re.U | re.M), "–\xa0"),
+        (re.compile(r"([^\d\w-])-([^\d\w-])", re.U), r"\1–\2"),
+        (re.compile("(\\d)[ \xa0](\\d{3})\\b", re.U), "\\1\u202f\\2"),
         (re.compile("[ \xa0]([!?:;])"), "\u202f\\1"),
-        (re.compile(r"(\d|\b)EUR(\d|\b)"), r"\1€\2"),
-        (re.compile(r"(\d|\b)GBP(\d|\b)"), r"\1£\2"),
-        (re.compile(r"(\d|\b)JPY(\d|\b)"), r"\1¥\2"),
-        (re.compile(r"\b(n)[o0°]\s*(\d)", re.I), "\\1º\xa0\\2"),
+        (re.compile(r"(\d|\b)EUR(\d|\b)", re.U), r"\1€\2"),
+        (re.compile(r"(\d|\b)GBP(\d|\b)", re.U), r"\1£\2"),
+        (re.compile(r"(\d|\b)JPY(\d|\b)", re.U), r"\1¥\2"),
+        (re.compile(r"\b(n)[o0°]\s*(\d)", re.U | re.I), "\\1º\xa0\\2"),
         (re.compile(r"oe(u|il)"), r"œ\1"),
         (re.compile(r"O(e|E)(u|U|il|IL)"), r"Œ\2"),
     ],
@@ -95,11 +90,9 @@ if spell:
                 "dictionary not found for language: {!r}".format(language))
         return text
 
+    @lru_cache(5)
     def get_dict(language=None):
         return spell.Dict(language)
-
-    if lru_cache:
-        get_dict = lru_cache(5)(get_dict)
 
     def fix_spelling(text, language=None):
         return get_dict(language).autofix(text)
@@ -111,5 +104,4 @@ else:
 def decode(buf: bytes, encoding=DEFAULT_ENCODING, language=None) -> str:
     """Decode and try to undo a downgraded transliteration.
     """
-    #text = unicodedata.normalize("NFKC", buf.decode(encoding, "replace"))
     return upgrade(buf.decode(encoding, "replace"), language)
