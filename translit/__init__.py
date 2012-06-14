4c6c7cb9700d61f9ada0d8d07cd8def108046222
spirit <spirit@localhost>
2012-06-14 19:41:55 +0200

    Minor

--- translit/__init__.py
+++ translit/__init__.py
@@ -1,20 +1,20 @@
 """Transliterate between Unicode and smaller coded character sets
 """
-# © 2012 spirit <hiddenspirit@gmail.com>
-# https://bitbucket.org/spirit/translit/
+#   © 2012 spirit <hiddenspirit@gmail.com>
+#   https://bitbucket.org/spirit/translit/
 #
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 3 of the License,
-# or (at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU Lesser General Public License as published
+#   by the Free Software Foundation, either version 3 of the License,
+#   or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Lesser General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
 
